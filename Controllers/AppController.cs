c734658fa1857c9072e145217f428813c9e9f1f1
Nazmih-Ibreash <nazmihibreash@gmail.com>
2022-02-08 16:42:29 +0200

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotels.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
