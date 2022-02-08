1b380235b46514ba5802ab2d73197749563885f3
nazmia-14 <nazmihibreash@gmail.com>
2022-02-08 23:17:18 +0200

    home-mvc

---
+++ Daata/Entities/Place.cs
@@ -0,0 +1,13 @@
+﻿namespace Hotels.Daata.Entities
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public int Stars { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Img { get; set; }
+    }
+}
